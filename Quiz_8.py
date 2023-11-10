3a51e048a2a8df5783e7e7ba80d5a3be83f7d9a8
dongcheolju <doungchul@gmail.com>
2023-11-10 16:40:04 +0900

    Update Quiz_8.py

--- Quiz_8.py
+++ Quiz_8.py
@@ -1,11 +1,10 @@
 import random
 
 def lotto_numbers():
-    numbers = random.sample(range(1, 46), 6) #중복 없는 함수
+    numbers = sorted(random.sample(range(1, 46), 6)) #중복 없는 함수
     result = f"생성된 로또 번호는 {numbers} 입니다."
     return numbers, result
 
-# 로또 번호 생성 및 출력
 numbers, output_string = lotto_numbers()
 print(output_string)
 
